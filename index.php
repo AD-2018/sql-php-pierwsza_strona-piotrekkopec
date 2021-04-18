881110afb03ff01dc23a03ccc32f4dda911c73af
Piotr Kopeć <piotrek_kopec2222@wp.pl>
2021-04-19 00:48:30 +0200

    x

--- index.php
+++ index.php
@@ -2,10 +2,33 @@
 <html>
 <head>
 <link rel="stylesheet" href="style.css">
-<title>Piotr Kopeć</title>
-
-
-
-
+<title>Piotr Kopec</title>
 </head>
+<body> 
+     <h1>Piotr Kopec</h1>
+ <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-piotrekkopec">Github</a>
+
+<div class="nav">
+   <a href="Pracownicy/pracownicy_organizacja.php"> Pracownicy i organizacja</a><br>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
 </html>
