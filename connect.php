c0d288a08e033f82c63b10ecf00ea284d8dd6c4c
Piotr Kopeć <piotrek_kopec2222@wp.pl>
2021-04-18 22:16:34 +0200

    x

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php
+$servername = $_SERVER["servername"];
+$username = $_SERVER["username"];
+$password = $_SERVER["password"];
+$dbname = $_SERVER["dbname"];
+
+$conn = new mysqli ($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+?>
