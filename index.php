b49e71d1ad449be336a2a5d4b4fa74967143ce1e
Piotr Kopeć <piotrek_kopec2222@wp.pl>
2021-04-19 08:56:53 +0200

    x

--- index.php
+++ index.php
@@ -9,9 +9,17 @@
  <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-piotrekkopec">Github</a>
 
 <div class="nav">
+   <br>
+   <a href="index.php">Strona Glowna</a><br>
+   <br>
    <a href="Pracownicy/pracownicy_organizacja.php"> Pracownicy i organizacja</a><br>
-   <h2> </h2>
-
-
+   <br>
+   <a href="pracownicy/funkAgregujaca.php">Funkcje agregujace</a><br>
+   <br>
+   <a href="pracownicy/dataczas.php">Data i czas</a><br>
+   <br>
+   <a href="pracownicy/formularz.html">Formularz</a><br>
+   <br>
+   <a href="pracownicy/danedobazy.php">Dane do bazy</a><br>
 </body>
 </html>
