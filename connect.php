95df9f0e17c58e0e144b24cce6d62e502c86090a
Piotr Kopeć <piotrek_kopec2222@wp.pl>
2021-06-13 23:15:26 +0200

    x

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
-$servername = $_SERVER["servername"];
-$username = $_SERVER["username"];
-$password = $_SERVER["password"];
-$dbname = $_SERVER["dbname"];
+$servername ="mysql-piotrkopec.alwaysdata.net";
+$username = "232845";
+$password = "Piotrek1825";
+$dbname = "piotrkopec_1";
 
 $conn = new mysqli ($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
--- index.php
+++ index.php
@@ -1,25 +1,19 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<link rel="stylesheet" href="style.css">
-<title>Piotr Kopec</title>
+    <link rel="stylesheet" href="style.css">
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
 </head>
-<body> 
-     <h1>Piotr Kopec</h1>
- <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-piotrekkopec">Github</a>
-
-<div class="nav">
-   <br>
-   <a href="index.php">Strona Glowna</a><br>
-   <br>
-   <a href="Pracownicy/pracownicy_organizacja.php"> Pracownicy i organizacja</a><br>
-   <br>
-   <a href="pracownicy/funkAgregujaca.php">Funkcje agregujace</a><br>
-   <br>
-   <a href="pracownicy/dataczas.php">Data i czas</a><br>
-   <br>
-   <a href="pracownicy/formularz.html">Formularz</a><br>
-   <br>
-   <a href="pracownicy/danedobazy.php">Dane do bazy</a><br>
+<body>
+<div class="container">
+      <div class="a">a</div>
+      <div class="b">b</div>
+      <div class="c">c</div>
+      <div class="d">d</div>
+</div>
+   
 </body>
 </html>
