b13399d8ab7666c58266c88468d41c50e947dc32
Piotr Kopeć <piotrek_kopec2222@wp.pl>
2021-06-13 23:42:28 +0200

    xd

--- index.php
+++ index.php
@@ -9,11 +9,31 @@
 </head>
 <body>
 <div class="container">
-      <div class="a">a</div>
+      <div class="a">
+      <form action="index.php"></form>
+      <input type="text">
+      <input type="text">
+      <input type="submit" values="dodaj">
+
+      <?php
+      require_once("conn.php");
+      $sql="SELECT * from pracownicy";
+      $result= mysqli_query($conn, $sql);
+      echo("table border='1'>");
+      echo("<th>Id</th><th>Imie</th>");
+      while($row=mysqli_fetch_assoc($result)){
+            echo('<tr>');
+            echo('<td>'.$row['id_pracownicy'].'</td>'.'<td>'.$row['imie'].'</td>');
+            echo('</tr>');
+      }
+      echo("</table>");
+      ?>
+      </div>
       <div class="b">b</div>
       <div class="c">c</div>
       <div class="d">d</div>
 </div>
+
    
 </body>
 </html>
