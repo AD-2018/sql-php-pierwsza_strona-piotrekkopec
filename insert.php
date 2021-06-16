37a036325babf4c6514b97eb3877f7556f3d3d04
Piotr Kopeć <piotrek_kopec2222@wp.pl>
2021-06-16 23:26:47 +0200

    a

--- insert.php
+++ insert.php
@@ -2,13 +2,5 @@
 require_once("connect.php");
 $sql="INSERT INTO pracownik (imie, dzial, zarobki, data_urodzenia)
 VALUES ('".$_POST['imie']."', '".$_POST['dzial']."','".$_POST['zarobki']."','".$_POST['data_urodzenia']."')";
-if ($conn->query($sql) === TRUE) {
-    echo "<br>New record created successfully";
-    header('Location: https://mojastronaa.herokuapp.com/Strona1.php');
-  } else {
-  //informacja o ewentualnych błędach
-    echo "Error: " . $sql . "<br>" . $conn->error;
-  }
-  
-  $conn->close();
+
 ?>
