d7b057be1945bd50f9a4df31a6882b2c034ac9c5
Piotr Kopeć <piotrek_kopec2222@wp.pl>
2021-06-16 22:45:24 +0200

    lol

--- Strona1.php
+++ Strona1.php
@@ -12,9 +12,35 @@
 <div class="a"></div>
 <div class="b"></div>
 <div class="c"></div>
-<div class="d"></div>
+<div class="d">
+<form action="insert.php" method="POST">
+<input type="text" name="imie">
+<input type="number" name="dzial">
+<input type="number" name="zarobki">
+<input type="date" name="data urodzenia">
+<input type="submit" name="Dodaj">
+</form>
+<?php
+require_once("connect.php");
+$sql="SELECT * from pracownik";
+$result=mysqli_query($conn, $sql);
+echo("<table border='1'>");
+echo('<th>ID</th><th>Imie</th><th>Dzial</th><th>Zarobki</th><th>Data Urodzenia</th>');
+while($row=mysqli_fetch_assoc($result)){
+    echo('<tr>');
+    echo('<td>'.$row['id_pracownicy'].'</td><td>'.$row['imie'].'</td><td>'.$row['dzial'].'</td><td>'.$row['zarobki'].'</td><td>'.$row['data_urodzenia'].'</td>');
+    echo('</tr>');
+}
+echo('</table>');
 
-</div>
-    
+
+
+
+
+
+
+
+?>
+</div> 
 </body>
 </html>
