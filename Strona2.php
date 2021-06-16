180bc499690bbebb0977731c7b3eee6059a53aa1
Piotr Kopeć <piotrek_kopec2222@wp.pl>
2021-06-16 23:56:44 +0200

    a

--- Strona2.php
+++ Strona2.php
@@ -9,7 +9,21 @@
 </head>
 <body>
 <div class="container">
-<div class="a"></div>
+<div class="a">
+<?php
+require_once("connect.php");
+$sql="SELECT * FROM country where Continent='Africa'";
+$result=mysqli_query($conn, $sql);
+echo("<table border='1'>");
+echo('<th>Name</th><th>Continent</th><th>Region</th><th>Population</th>');
+while($row=mysqli_fetch_assoc($result)){
+    echo('<tr>');
+    echo('<td>'.$row['Name'].'</td><td>'.$row['Continent'].'</td><td>'.$row['Region'].'</td><td>'.$row['Population'].'</td>');
+    echo('</tr>');
+}
+echo('</table>');
+?>
+</div>
 <div class="b"></div>
 <div class="c"></div>
 <div class="d">
