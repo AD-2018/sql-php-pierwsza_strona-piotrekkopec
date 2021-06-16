f91eb06377555176fb1a9c8f4b3af58a380379d9
Piotr Kopeć <piotrek_kopec2222@wp.pl>
2021-06-16 23:21:03 +0200

    l

--- insert.php
+++ insert.php
@@ -1,7 +1,7 @@
 <?php
 require_once("connect.php");
 $sql="INSERT INTO pracownik (id_pracownicy, imie, dzial, zarobki, data_urodzenia)
-VALUES (null, '".$_POST['imie']."', '".$_POST['dzial']."','".$_POST['zarobki']."','".$_POST['data_urodzenia']."')";
+VALUES ( '".$_POST['imie']."', '".$_POST['dzial']."','".$_POST['zarobki']."','".$_POST['data_urodzenia']."')";
 if ($conn->query($sql) === TRUE) {
     echo "<br>New record created successfully";
     header('Location: https://mojastronaa.herokuapp.com/Strona1.php');
