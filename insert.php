fdda76e41e4510cf7d86fc6c88b02abaa2c2a9e5
Piotr Kopeć <piotrek_kopec2222@wp.pl>
2021-06-16 23:34:38 +0200

    a

--- insert.php
+++ insert.php
@@ -1,6 +1,6 @@
 <?php
 require_once("connect.php");
-$sql="INSERT INTO pracownik (id_pracownicy, imie, dzial, zarobki, data_urodzenia)
-VALUES ('".$_POST['id']."', '".$_POST['imie']."', '".$_POST['dzial']."','".$_POST['zarobki']."','".$_POST['data_urodzenia']."')";
+$sql="INSERT INTO pracownik (imie, dzial, zarobki, data_urodzenia)
+VALUES ('".$_POST['imie']."', '".$_POST['dzial']."','".$_POST['zarobki']."','".$_POST['data_urodzenia']."')";
 
 ?>
