8a6704e1b7a02c6c27386e21d2dded6b0e196c5f
Piotr Kopeć <piotrek_kopec2222@wp.pl>
2021-06-17 12:40:55 +0200

    a

--- insert2.php
+++ insert2.php
@@ -3,6 +3,6 @@ require_once("connect.php");
 $sql="INSERT INTO country ('Name', Continent, Region, 'Population')
 VALUES('".$_POST['Name']."','".$_POST['Continent']."','".$_POST['Region']."','".$_POST['Population']."' )";
 if ($conn->query($sql) === TRUE) {
-	header('Location: strona3.php');
+	header('Location: Strona3.php');
 }
 ?>
