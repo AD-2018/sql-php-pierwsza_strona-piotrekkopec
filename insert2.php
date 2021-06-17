75da2df984b7b13b3756cbfc2b7d574e36a62951
Piotr Kopeć <piotrek_kopec2222@wp.pl>
2021-06-17 12:43:18 +0200

    a

--- insert2.php
+++ insert2.php
@@ -3,6 +3,7 @@ require_once("connect.php");
 $sql="INSERT INTO country ('Name', Continent, Region, 'Population')
 VALUES('".$_POST['Name']."','".$_POST['Continent']."','".$_POST['Region']."','".$_POST['Population']."' )";
 if ($conn->query($sql) === TRUE) {
-	header('Location: Strona3.php');
+	header('Location: https://mojastronaa.herokuapp.com/Strona3.php');
 }
+$conn->close();
 ?>
