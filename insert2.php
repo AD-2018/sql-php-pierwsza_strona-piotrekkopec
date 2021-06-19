da0d9d3ce9cfa29a69ff64585a74eb775f383c85
Piotr Kopeć <piotrek_kopec2222@wp.pl>
2021-06-19 10:35:19 +0200

    a

--- insert2.php
+++ insert2.php
@@ -4,6 +4,9 @@ $sql="INSERT INTO country (Name, Continent, Region, Population)
 VALUES('".$_POST['Name']."','".$_POST['Continent']."','".$_POST['Region']."','".$_POST['Population']."' )";
 if ($conn->query($sql) === TRUE) {
 	header('Location: https://mojastronaa.herokuapp.com/Strona3.php');
+}else{
+	echo "Error: " .$sql . "<br>" . $conn->error;
 }
+
 $conn->close();
 ?>
